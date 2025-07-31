c0810e18f05b512daba3e8cc54efffef5bd96b6c
jasons1142 <jasons11@bu.edu>
2025-07-31 12:35:24 -0400

    Adding useState hooks for each aspect of the form

--- app/Add/page.tsx
+++ app/Add/page.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useState } from "react";
 
 export default function Add() {
-
+    const [title, setTitle] = useState("");
+    const [link, setLink] = useState("");
+    const [topics, setTopics] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+    const [dateCompleted, setDateCompleted] = useState("")
 
 
     return (
@@ -13,23 +19,40 @@ export default function Add() {
 
                     <div className = "flex flex-col items-center">
                         <label>Problem Name</label>
-                        <input type = "text" name = "problemText" required className = "border border-black"/>
+                        <input 
+                            type = "text" 
+                            name = "problemText" 
+                            required 
+                            className = "border border-black"
+                            value = {title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
                     </div>
 
                     
                     <div className = "flex flex-col items-center">
                         <label>Problem Link</label>
-                        <input type = "text" className="border border-black"/>
+                        <input 
+                            type = "text" 
+                            className="border border-black"
+                            value = {link}
+                            onChange={(e) => setLink(e.target.value)}
+                        />
                     </div>
 
                     <div className = "flex flex-col items-center">
                         <label>Topics</label>
-                        <input type = "text" className="border border-black"/>
+                        <input 
+                            type = "text" 
+                            className="border border-black"
+                            value = {topics}
+                            onChange={(e) => setTopics(e.target.value)}
+                        />
                     </div>
 
                     <div className = "flex flex-col items-center">
                         <label>Problem Difficulty</label>
-                        <select className = "border border-black">
+                        <select className = "border border-black" value = {difficulty} onChange = {(e) => setDifficulty(e.target.value)}>
                             <option value="">--Please choose an option--</option>
                             <option value="Easy">Easy</option>
                             <option value="Medium">Medium</option>
@@ -39,10 +62,15 @@ export default function Add() {
 
                     <div className = "flex flex-col items-center">
                         <label>Date Completed</label>
-                        <input type = "date" className="border border-black"/>
+                        <input 
+                            type = "date" 
+                            className="border border-black"
+                            value = {dateCompleted}
+                            onChange = {(e) => setDateCompleted(e.target.value)}
+                        />
                     </div>
 
-                    <button className = "border border-black bg-gray-400 rounded-md">Submit</button>
+                    <button type = "submit" className = "border border-black bg-gray-400 rounded-md">Submit</button>
                 </form>
             </main>
             <Footer />
