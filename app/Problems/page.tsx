ae36902635d45e2b3a8198836f7073e26a951e63
jasons1142 <jasons11@bu.edu>
2025-08-07 11:32:50 -0400

    initialization of problems page

---
+++ app/Problems/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { useState } from "react";
+
+export default function Problem() {
+    return (
+        <div>
+            <main className = "bg-stone-200 min-h-screen">
+                <Navbar />
+            </main>
+            <Footer />
+        </div>
+        
+    )
+}
